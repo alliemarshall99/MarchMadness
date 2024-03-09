280e9b55631e9e851ace176ca91c75256b9574c0
unknown <alliemarshall99@gmail.com>
2024-03-08 22:09:23 -0800

    functions working properly

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,57 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import collegeBasketballTeams from './CollegeBasketballTeams.json';
+
+interface Team {
+  school: string;
+  name: string;
+  city: string;
+  state: string;
+}
+
+function MarchMadnessHeading() {
+  return (
+    <div>
+      <h1>Welcome to March Madness!</h1>
+      <h3>Learn more about your favorite teams!</h3>
+    </div>
+  );
+}
+
+class TeamCard extends React.Component<{ team: Team }> {
+  render() {
+    const { school, name, city, state } = this.props.team;
+
+    return (
+      <div>
+        <h1>{school}</h1>
+        <h3>{name}</h3>
+        <h3>{`${city}, ${state}`}</h3>
+      </div>
+    );
+  }
+}
+
+class TeamList extends React.Component<{ teams: Team[] }> {
+  render() {
+    const { teams } = this.props;
+
+    return (
+      <div>
+        {teams.map((team, index) => (
+          <TeamCard key={index} team={team} />
+        ))}
+      </div>
+    );
+  }
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MarchMadnessHeading />
+      <TeamList teams={collegeBasketballTeams.teams} />
     </div>
   );
 }
